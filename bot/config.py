acf7dfbfb478d517f4e1cb1da6486c54b118ed52
Lunarmagpie <Bambolambo0@gmail.com>
2022-06-14 18:24:26 -0400

    format with black

--- bot/config.py
+++ bot/config.py
@@ -11,5 +11,5 @@ CONFIG = Config(
     **{
         key.lower() if key else None: value
         for key, value in dotenv.dotenv_values(".env").items()
-    } # type: ignore
+    }  # type: ignore
 )
